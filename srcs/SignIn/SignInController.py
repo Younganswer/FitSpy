0f032fe877ffef872a5af34450eaa0f997f8ab4e
Younganswer <dudtj1999@gmail.com>
2023-05-19 21:12:50 +0900

    [Feat] class method #4
    On branch Feat/SignUp&SignIn#4
    Changes to be committed:
    	modified:   srcs/SignIn/SignInController.py

--- srcs/SignIn/SignInController.py
+++ srcs/SignIn/SignInController.py
@@ -2,8 +2,6 @@ class SignInController:
 	def __init__(self, window):
 		pass
 
+	@classmethod
 	def validate(self, identity, password):
 		return (identity == "admin" and password == "password")
-
-	def showFrame(self, className):
-		print("Sign Up button clicked")
