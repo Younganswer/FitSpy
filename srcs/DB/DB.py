55ce04e5245c0a9b7af1449d2a37096b01c76249
Younganswer <dudtj1999@gmail.com>
2023-05-29 22:00:43 +0900

    .

--- srcs/DB/DB.py
+++ srcs/DB/DB.py
@@ -3,18 +3,13 @@ import pickle
 class DB:
 	__user_data = []
 
-	def __init__(self):
+	def	__init_DB():
 		try:
-			with open("srcs/DB/user_data.pickle", "rb") as fr:
-				DB.__user_data = pickle.load(fr)
-				fr.close()
-		except:
-			pass
-	
-	def	__del__(self):
-		with open("srcs/DB/user_data.pickle", "wb") as fw:
-			pickle.dump(DB.__user_data, fw)
-			fw.close()
+			with open("DB/user_data.pickle", "rb") as f:
+				DB.__user_data = pickle.load(f)
+		except FileNotFoundError:
+			DB.__user_data = []
+
 
 	@staticmethod
 	def get_user_data():
@@ -23,3 +18,5 @@ class DB:
 	@staticmethod
 	def	add_user_data(user_data):
 		DB.__user_data.append(user_data)
+		with open("DB/user_data.pickle", "wb") as f:
+			pickle.dump(DB.__user_data, f)
